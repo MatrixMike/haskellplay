085258a23156e42ba2a95899d97d2c0011395c49
Lyndon Maydwell <lyndon@sordina.net>
2020-10-08 17:26:35 +1100

    working arrow channels!

--- src/ChanArrow.hs
+++ src/ChanArrow.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE Arrows #-}
 
+-- | CSP like library: Arrow interface
+--
+-- TODO:
+-- 
+--   * Recovery/Retry capability
+--   * Fix await deadlock
+--   * Allow configurable parallelism
+--   * Generic Chan functions, then specific newtype
+--   * Different transport options, buffered, etc.
+-- 
 module ChanArrow where
 
 import Prelude hiding (id, (.))
@@ -9,37 +21,65 @@ import Control.Arrow
 import Control.Concurrent
 import Control.Category
 import Control.Concurrent.Async (wait, Async, async)
-import Control.Monad (void)
-import Data.Maybe (catMaybes, isJust)
+import Data.Maybe (isNothing, catMaybes)
 import Data.Void
 import Control.Applicative (Applicative(liftA2))
+import Data.Foldable (for_)
+import Control.Exception (Exception, SomeException, try)
+import GHC.Natural (Natural)
+import System.Random
+import Control.Monad (replicateM)
 
 main :: IO ()
-main = void do feedCharrow (arrA >>> arrB) [1..10] >>= print
+main = do
+    run test0 >>= wait
+    run test1 >>= wait
+    run test2 >>= wait
+    run test3 >>= wait
+    run test4 >>= wait
+    run test5 >>= wait
+    run (id >>> id) >>= wait
+    run id >>= wait
+    run ( sourceIO (\cb -> replicateM 3 do cb (5 :: Int) >> threadDelay 1000000)
+          >>> arr show
+          >>> sinkIO print
+        ) >>= wait
 
-test :: IO ()
-test = do
-    o <- withCharrow (arr succ >>> arrA) \i -> do
-        i 1
-        i 2
-    print o
+    return ()
 
-test2 :: IO ()
-test2 = do
-    o <- withCharrow ax \i -> do
-        i (1 :: Int)
-        i 2
-    print o
+test0 :: Charrow Void Void
+test0 = fmap id id
+
+test1 :: Charrow Void Void
+test1 = sourceList [1::Int,2] >>> arr succ >>> arr show >>> sinkIO putStrLn
+
+test2 :: Charrow Void Void
+test2 = sourceList [1::Int ..11] >>> prog >>> sinkIO print
     where
-        ax = proc x -> do
-            y <- arr succ -< x
-            id -< (x + y)
+    prog = proc i -> do
+        j <- arr succ  -< i
+        k <- arr show  -< j
+        l <- arr succ  -< j
+        m <- arr (> 5) -< j
+        n <- process (\x@(_x,_y,z) -> print x >> return z) -< (k,l,m)
 
-arrA :: Charrow Int String
-arrA = fmap show id
+        arr not -< n
 
-arrB :: Charrow String ()
-arrB = process print
+test3 :: Charrow Void Void
+test3 = sourceList [1 :: Int] >>> process print >>> sinkIO print
+
+test4 :: Charrow Void Void
+test4 = sourceList [1 :: Int] >>> (process print &&& process print) >>> sinkIO print
+
+test5 :: Charrow Void Void
+test5 = sourceList [1 :: Int ..10] >>> arr (0 :: Natural,) >>> processRetry' @SomeException 20 flakeyThing >>> sinkIO print
+    where
+    flakeyThing x = do
+        r <- randomRIO (1::Int,10)
+        if x < 3 || r > 5
+            then return x
+            else error ("oops! " <> show x)
+
 
 -- Data
 
@@ -47,65 +87,103 @@ data Charrow i o = Charrow { runCharrow :: IO (Chan (Maybe i), Chan (Maybe o), A
 
 -- Top level
 
-run :: Charrow i o -> IO (Async ())
-run c = do
-    (i,_o,a) <- runCharrow c
-    writeChan i Nothing
+run :: Charrow Void Void -> IO (Async ())
+run = run'
+
+-- | This is unsafe, since the pipeline may not be terminated
+run' :: Charrow i o -> IO (Async ())
+run' c = do
+    -- Compose an empty sourceList to ensure termination
+    (_i,_o,a) <- runCharrow (sourceList [] >>> c)
     return a
 
-source :: ((o -> IO ()) -> IO a2) -> Charrow Void o
-source m = Charrow do
+buildCharrow :: (Chan (Maybe i) -> Chan (Maybe o) -> IO ()) -> Charrow i o
+buildCharrow cb = Charrow do
     i <- newChan
     o <- newChan
-    a <- async do
-        m (writeChan o . Just)
-        writeChan o Nothing
+    a <- async do cb i o
     return (i,o,a)
 
-withCharrow :: Charrow i o -> ((i -> IO ()) -> IO ()) -> IO [o]
-withCharrow c cb = do
-    (i,o,a) <- runCharrow c
-    cb (writeChan i . Just)
-    writeChan i Nothing
-    c2l o
+sourceList :: Foldable t => t o -> Charrow Void o
+sourceList = sourceIO . for_
 
-feedCharrow :: Charrow i o -> [i] -> IO [o]
-feedCharrow c l = do
-    (i, o, a) <- runCharrow c
-    async do l2c i l >> writeChan i Nothing
-    c2l o
+sourceIO :: ((o -> IO ()) -> IO a2) -> Charrow Void o
+sourceIO cb =
+    buildCharrow \_i o -> do
+        cb (writeChan o . Just)
+        writeChan o Nothing
+
+sinkIO :: (o -> IO ()) -> Charrow o Void
+sinkIO cb =
+    buildCharrow \i _o -> do
+        mapM_ cb =<< c2l i
 
 process :: (a -> IO b) -> Charrow a b
-process f = Charrow do
-    i <- newChan
-    o <- newChan
-    a <- async do
-        cs <- getChanContents i
-        flip mapM_ cs \x -> do
+process f =
+    buildCharrow \i o -> do
+        cs <- c2l' i
+        for_ cs \x -> do
+            -- TODO: Figure out how to eliminate the case naturally
             case x of
                 Nothing -> writeChan o Nothing
-                Just x' -> f x' >>= writeChan o . Just
-    return (i,o,a)
+                Just y  -> writeChan o . Just =<< f y
+
+processN :: (a -> IO [b]) -> Charrow a b
+processN f =
+    buildCharrow \i o -> do
+        cs <- c2l' i
+        for_ cs \x -> do
+            -- TODO: Figure out how to eliminate the case naturally
+            case x of
+                Nothing -> writeChan o Nothing
+                Just y  -> mapM_ (writeChan o . Just) =<< f y
+
+processRetry :: Natural -> (i -> IO o) -> Charrow i o
+processRetry maxRetries f = arr (0,) >>> processRetry' @SomeException maxRetries f >>> rights
+
+rights :: Charrow (Either a b) b
+rights = mapN (either (const []) pure)
+
+mapN :: (a -> [b]) -> Charrow a b
+mapN f = processN (return . f)
+
+processRetry' :: Exception e => Natural -> (a -> IO o) -> Charrow (Natural, a) (Either e o)
+processRetry' maxRetries f =
+    buildCharrow \i o -> do
+        cs <- c2l' i
+        for_ cs \x -> do
+            -- TODO: Figure out how to eliminate the case naturally
+            case x of
+                Nothing -> writeChan o Nothing
+                Just (n, y) -> do
+                    r <- try do f y
+                    case r of
+                        Right _ -> writeChan o (Just r)
+                        Left  err
+                            | n >= maxRetries -> return ()
+                            | otherwise       -> do
+                                writeChan i (Just (succ n, y))
+                                writeChan o (Just (Left err))
 
 -- Instances
 
 instance Functor (Charrow a) where
     fmap f c = Charrow do
         (i,o,a) <- runCharrow c
-        o' <- newChan
-        a' <- async do
-            getChanContents o >>= writeList2Chan o' . fmap (fmap f)
+        o'  <- newChan
+        a'  <- async do
+            c2c f o o'
             wait a
         return (i,o',a')
 
 instance Category Charrow where
-    id    = Charrow $ async (return ()) >>= \a -> newChan >>= \c -> return (c,c,a)
+    id    = Charrow do async (return ()) >>= \a -> newChan >>= \c -> return (c,c,a)
     g . f = Charrow do
         (fi, fo, fa) <- runCharrow f
         (gi, go, ga) <- runCharrow g
 
         a <- async do
-            getChanContents fo >>= writeList2Chan gi
+            c2c id fo gi
             wait fa
             wait ga
 
@@ -118,17 +196,17 @@ instance Arrow Charrow where
         (i,o,a) <- runCharrow c
         i'      <- newChan
         o'      <- newChan
-        is      <- getChanContents i'
-        os      <- getChanContents o
+        is      <- c2l' i'
+        os      <- c2l' o
 
-        a' <- async do writeList2Chan i (map (fmap fst) is)
-
-        a'' <- async do
-            writeList2Chan o' (zipWith (liftA2 (,)) os (map (fmap snd) is))
+        a'   <- async do writeList2Chan i (map (fmap fst) is)
+        a''  <- async do writeList2Chan o' (zipWith (liftA2 (,)) os (map (fmap snd) is))
+        a''' <- async do
             wait a
             wait a'
+            wait a''
 
-        return (i',o',a'')
+        return (i',o',a''')
 
 -- Helpers
 
@@ -136,4 +214,20 @@ l2c :: Chan (Maybe a) -> [a] -> IO ()
 l2c c = writeList2Chan c . fmap Just
 
 c2l :: Chan (Maybe a) -> IO [a]
-c2l c = catMaybes . takeWhile isJust <$> getChanContents c
+c2l = fmap catMaybes . c2l'
+
+c2l' :: Chan (Maybe a) -> IO [Maybe a]
+c2l' c = takeUntil isNothing <$> getChanContents c
+
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ []     = []
+takeUntil p (x:xs)
+    | p x       = [x]
+    | otherwise = x : takeUntil p xs
+
+c2c :: (a1 -> a2) -> Chan (Maybe a1) -> Chan (Maybe a2) -> IO ()
+c2c f i o = do
+    l <- c2l i
+    l2c o (fmap f l)
+    writeChan o Nothing
+
